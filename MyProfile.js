06a3134c07fb3ff924b9a664d7ebffa3112631a2
Maitrayee Datta <maitrayee.datta58@gmail.com>
2017-11-07 23:19:52 +0530

    Profile Uploaded

---
+++ MyProfile.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    $("#HomeMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#Home").offset().top
+        }, 1000);
+    });
+
+    $("#SkillSetMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#SkillSet").offset().top
+        }, 1000);
+    });
+
+    $("#AboutMeMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#AboutMe").offset().top
+        }, 1000);
+    });
+
+    $("#ContactDetailsMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#ContactDetails").offset().top
+        }, 1000);
+    });
+
+    $("#ContactDetailsMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#ContactDetails").offset().top
+        }, 1000);
+    });
+
+    $("#EducationDetailsMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#EducationDetails").offset().top
+        }, 1000);
+    });
+
+    $("#SkillSetMenu").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#SkillSet").offset().top
+        }, 1000);
+    });
+
+});
